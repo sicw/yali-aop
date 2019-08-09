60cae973399b2b50f2eb422c552330ac3e222b35
sicwen <1224260992@qq.com>
2019-08-09 20:56:36 +0800

    fix

--- src/main/java/com/maoyan/demo/DefaultAdvisorChainFactory.java
+++ src/main/java/com/maoyan/demo/DefaultAdvisorChainFactory.java
@@ -1,5 +1,7 @@
 package com.maoyan.demo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -7,17 +9,13 @@ import java.util.List;
  * @date 2019/08/09
  */
 public class DefaultAdvisorChainFactory {
-
-
-
-
     public List<Object> getInterceptorsAndDynamicInterceptionAdvice(List<Advisor> advisors){
+        List<Object> result = new ArrayList<>();
+        DefauleAdvisorAdapterRegistry registry = new DefauleAdvisorAdapterRegistry();
         for (Advisor advisor : advisors) {
-
+            MethodInterceptor[] interceptors = registry.getInterceptors(advisor);
+            result.addAll(Arrays.asList(interceptors));
         }
-
-
-        return null;
+        return result;
     }
-
 }
