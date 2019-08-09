417fa28210da563d32c0ed56572c7af46f4ef7c3
sicwen <1224260992@qq.com>
2019-08-09 21:36:34 +0800

    fix

--- src/main/java/com/maoyan/demo/TargetBean.java
+++ src/main/java/com/maoyan/demo/TargetBean.java
@@ -1,11 +1,13 @@
 package com.maoyan.demo;
 
+import com.maoyan.demo.annotation.Aspect;
 import com.maoyan.demo.annotation.Before;
 
 /**
  * @author sichengwen
  * @date 2019/08/09
  */
+@Aspect(value = "test")
 public class TargetBean {
     @Before
     public void doWork(){
--- src/main/java/com/maoyan/demo/annotation/Aspect.java
+++ src/main/java/com/maoyan/demo/annotation/Aspect.java
@@ -1,9 +1,16 @@
 package com.maoyan.demo.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author sichengwen
  * @date 2019/08/02
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Aspect {
     String value() default "";
 }
